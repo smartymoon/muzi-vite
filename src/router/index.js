a42e88c0acc4256e6320ce137612bb00eedcf927
kongzi <image8870@gmail.com>
2021-08-16 17:22:26 +0800

    c

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = new createRouter({
-	history:createWebHashHistory('/mobile/'),
+	history:createWebHistory('/mobile/'),
 	routes:[
 
 		// ----------主页----------
