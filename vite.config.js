9847e5920b4880d1a341eb49b56a60a28f4f43ec
kongzi <image8870@gmail.com>
2021-09-01 15:49:03 +0800

    add uc

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import styleImport from 'vite-plugin-style-import';
+import styleImport from 'vite-plugin-style-import'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
   base: './',
   plugins: [
     vue(),
+    legacy({
+      polyfills: ['es.promise.finally', 'es/map', 'es/set'],
+      modernPolyfills: ['es.promise.finally']
+    }),
     styleImport({
       libs: [
         {
