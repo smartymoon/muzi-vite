c638b9a24510ae0518ba586cf2263a1d380be9f3
kongzi <image8870@gmail.com>
2021-09-03 16:00:44 +0800

    c

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import styleImport from 'vite-plugin-style-import'
 
 export default defineConfig({
   base: './',
   plugins: [
+    legacy ({ 
+      targets : [ 'ie >= 11' ] , 
+      additionalLegacyPolyfills : [ 'regenerator-runtime/runtime' ] 
+    }), 
     vue(),
     styleImport({
       libs: [
